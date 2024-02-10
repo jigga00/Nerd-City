3f178cd82c87500b3e803f60da1b9af12227fb34
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-10 13:09:30 -0500

    auth.js

---
+++ nerd-city-backend/src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, 'your-secret-key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+module.exports = { authenticateUser };
