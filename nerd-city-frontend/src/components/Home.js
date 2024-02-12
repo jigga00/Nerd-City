d72e9555c92db83ddc5dec5a80a42575a91f0a2f
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-12 02:00:28 -0500

    Home.js

---
+++ nerd-city-frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Nerd City!</h2>
+      {/* Add content for the home page */}
+    </div>
+  );
+};
+
+export default Home;
