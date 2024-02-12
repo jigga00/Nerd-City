7d39692ba308e118bfd8da61b9b521630c74b43c
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-12 02:13:32 -0500

    Add files via upload

---
+++ nerd-city-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
