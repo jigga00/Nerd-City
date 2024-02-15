5c7c7ba32c854b63ad0ea698d17f021aa09b5609
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-15 03:29:19 -0500

    App.js

--- nerd-city-frontend/src/App.js
+++ nerd-city-frontend/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
-
+import FriendsList from './components/FriendsList';
+import Chat from './components/Chat';
+// import SignInSignUpPage from './components/SignInSignUp';
 
 function App() {
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
+      <Routes>
+        {/* <Route path="/signin" element={<SignInSignUpPage />} /> */}
+         (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/friends" element={<FriendsList />} />
+            <Route path="/chat" element={<Chat />} />
+          </>
+        ) 
+      </Routes>
     </Router>
   );
 }
