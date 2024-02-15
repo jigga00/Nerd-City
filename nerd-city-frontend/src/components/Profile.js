bb2b75581561a0a59e7fa8b2b81625174ec57573
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-15 03:40:05 -0500

    Profile.js

--- nerd-city-frontend/src/components/Profile.js
+++ nerd-city-frontend/src/components/Profile.js
@@ -1,32 +1,5 @@
 import React, { useState } from 'react';
-import { uploadPictureAndStatement } from '../api';
-
-const fetchFlickrAPI = async () => {
-  const encodedParams = new URLSearchParams();
-  encodedParams.set('accessToken', '<REQUIRED>');
-  encodedParams.set('photosetId', '<REQUIRED>');
-  encodedParams.set('accessSecret', '<REQUIRED>');
-  encodedParams.set('commentText', '<REQUIRED>');
-
-  const url = 'https://flickrdidenkoradionv1.p.rapidapi.com/addComment';
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-      'X-RapidAPI-Host': 'FlickrdidenkoradionV1.p.rapidapi.com'
-    },
-    body: encodedParams
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-};
+import FriendsList from './FriendsList';
 
 const Profile = () => {
   const [picture, setPicture] = useState(null);
@@ -41,22 +14,8 @@ const Profile = () => {
   };
 
   const handleSubmit = () => {
-    const data = new FormData();
-    data.append('picture', picture);
-    data.append('statement', statement);
-
-    uploadPictureAndStatement(data)
-      .then(response => {
-        console.log(response);
-        // Handle success
-
-        // Make Flickr API call after successful picture and statement upload
-        fetchFlickrAPI();
-      })
-      .catch(error => {
-        console.error(error);
-        // Handle error
-      });
+    // You can use 'picture' and 'statement' here if needed
+    console.log('Submit:', picture, statement);
   };
 
   return (
@@ -73,6 +32,7 @@ const Profile = () => {
           <textarea value={statement} onChange={handleStatementChange} />
         </label>
         <br />
+        <FriendsList />
         <button type="button" onClick={handleSubmit}>
           Save Changes
         </button>
