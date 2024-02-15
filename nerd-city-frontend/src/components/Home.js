ee5db4bac56fc80590cd72ce8d18976b7471087a
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-15 03:38:13 -0500

    Home.js

--- nerd-city-frontend/src/components/Home.js
+++ nerd-city-frontend/src/components/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Navbar from './Navbar';
+import Chat from './Chat';
+import FriendsList from './FriendsList';
+import PostForm from './PostForm'; // Import the PostForm component
+import SignInSignUpPage from './SignInSignUp';
 
 const Home = () => {
+  // Define a function to handle post submission
+  const handlePostSubmit = () => {
+    // Add any logic you want to perform after a post is submitted
+    console.log('Post submitted!');
+  };
+
   return (
     <div>
+      <Navbar />
       <h2>Welcome to Nerd City!</h2>
-      {/* Add content for the home page */}
+      <Chat />
+      <FriendsList />
+    
+      <PostForm onPostSubmit={handlePostSubmit} />
+      
+      <SignInSignUpPage />
+      {/* Add other content for the home page */}
     </div>
   );
 };
